195922f8f8b1838350bfcb4c50710a07e7903082
anugoyal998 <anugoyal998@gmail.com>
2023-04-20 11:03:51 +0530

    Home Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +19,11 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
-          {children}
-          
-          </Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          {/* <Toaster position="bottom-right" /> */}
+          <main>{children}</main>
+        </Providers>
         {/* Allow for more height on mobile devices */}
         <div className="h-40 md:hidden" />
       </body>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { FC } from "react";
+import { buttonVariants } from "@/ui/Button";
+import SignInButton from "@/components/SignInButton";
+import SignOutButton from "@/components/SignOutButton";
+import ThemeToggle from "@/components/ThemeToggle";
 
-interface NavbarProps {}
-
-const Navbar = async ({}) => {
+const Navbar = async () => {
   const session = await getServerSession();
   return (
     <div className="fixed backdrop:blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
-        <Link href="/" className={buttonVariants({ variants: "link" })}>
+        <Link href="/" className={buttonVariants({ variant: "link" })}>
           Text Similarity 1.0
         </Link>
         <div className="md:hidden">
@@ -20,13 +22,13 @@ const Navbar = async ({}) => {
           <ThemeToggle />
           <Link
             href="/documentation"
-            className={buttonVariants({ variants: "ghost" })}
+            className={buttonVariants({ variant: "ghost" })}
           >
             Documentation
           </Link>
           {session ? (
             <>
-              <Link className={buttonVariants({ variants: "ghost" })}></Link>
+              <Link href="/dashboard" className={buttonVariants({ variant: "ghost" })}></Link>
               <SignOutButton />
             </>
           ) : (
