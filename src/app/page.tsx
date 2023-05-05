555818cc4070edb79212f587b155464a9678f2bf
anugoyal998 <anugoyal998@gmail.com>
2023-05-05 11:32:35 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,37 +12,38 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
-      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
-        <div className="h-full mt-[10vh] gap-6 flex justify-start lg:justify-center items-center lg:items-start">
-          <div>
-            <LargeHeading
-              size="lg"
-              className="three-d text-black dark:text-light-gold"
+      <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading
+            size="lg"
+            className="three-d text-black dark:text-light-gold"
+          >
+            Get Real-time <br /> Stock Analysis.
+          </LargeHeading>
+
+          <Paragraph className="max-w-xl lg:text-left">
+            With the React Finance API, you can easily analyse Stocks in
+            real-time with a free{" "}
+            <Link
+              href="/login"
+              className="underline underline-offset-2 text-black dark:text-light-gold"
             >
-              Get Real-time <br /> Stock Analysis
-            </LargeHeading>
-            <Paragraph className="max-w-xl lg:text-left">
-              With the React Finance API, you can easily analyse Stocks in
-              real-time with a free{" "}
-              <Link
-                href="/login"
-                className="underline underline-offset-2 text-black dark:text-light-gold"
-              >
-                API Key
-              </Link>
-            </Paragraph>
-          </div>
-          {/* <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
+              API key
+            </Link>
+            .
+          </Paragraph>
+
+          <div className="relative w-full max-w-xl lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
             <Image
               priority
-              className="img-shadow"
+              className="img-shadow "
               quality={100}
               style={{ objectFit: "contain" }}
               fill
-              src='/Snap.png'
-              alt='hero'
+              src="/Snap.png"
+              alt="typewriter"
             />
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
